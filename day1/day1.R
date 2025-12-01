c85d10524857130a5f7b63f8e6a40f482ab9b5e2
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-01 17:59:41 +0100

    part 2

--- day1/day1.R
+++ day1/day1.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 
-get_password <- function(file) {
+get_password <- function(
+    file, 
+    print_info = FALSE
+  ) {
+  # read input
   df <- 
     read_csv(file, col_names = "turn", show_col_types = FALSE) |> 
     mutate(
@@ -8,30 +12,50 @@ get_password <- function(file) {
       dist = parse_number(turn)
     )
   
+  # turn dial 
   counter <- 0
   position <- 50
+  previous_position <- 50
   for (i in seq_along(df$turn)) {
-    if (df$dir[i] == "L") {# left
-      pos_tmp <- position - df$dist[i]
-      if (pos_tmp >= 0) {# cross 0?
+    turns_mod <- df$dist[i] %% 100
+    # times passed 0 (hundreds)
+    zeros <- (df$dist[i] - turns_mod) / 100
+    
+    if (df$dir[i] == "L") {     # left
+      pos_tmp <- position - turns_mod
+      if (pos_tmp >= 0) {       # not cross 0
         position <- pos_tmp
-      } else {
+      } else {                  # cross 0
         position <- pos_tmp + 100
+        if (previous_position != 0) {
+          zeros <- zeros + 1
+        }
       }
-    } else {# right
-      pos_tmp <- position + df$dist[i]
-      if (pos_tmp <= 99) {# cross 0?
+    } else {                    # right
+      pos_tmp <- position + turns_mod
+      if (pos_tmp <= 99) {      # not cross 0
         position <- pos_tmp
-      } else {
+      } else {                  # cross 0
         position <- pos_tmp - 100
+        if (position != 0 & previous_position != 0) {
+          zeros <- zeros + 1
+        }
       }
     }
-    # message("Position after turn ", i, ": ", position) 
     
-    counter <- counter + (position == 0)
+    zeros <- zeros + (position == 0) # current position 0
+    if (print_info) {
+      message(
+        "Position after turn ", df$turn[i], ": ", position, 
+        " (", zeros, " zeros)"
+      )
+    }
+    
+    counter <- counter + zeros
+    previous_position <- position 
   }
   message("The password is ", counter)
 }
 
-get_password("day1/input_example.txt")
+get_password("day1/input_example.txt", print_info = TRUE)
 get_password("day1/input.txt")
