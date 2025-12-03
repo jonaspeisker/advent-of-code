58ee85d80eb47df886418974eb44acaf94c4ad5c
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-03 10:09:04 +0100

    format day 2

--- day2/day2.R
+++ day2/day2.R
@@ -2,34 +2,46 @@ invalid_id_sum <- function(file_name, pattern) {
   input_raw <- readLines(file_name, warn = F) 
   # parse input
   input_sep <- strsplit(input_raw, ",")[[1]]
-  input_sep2 <- strsplit(input_sep, "-")
-  # expand values and check
+  input_sep2 <- strsplit(input_sep, "-") 
+  # expand sequence based on start and stop value
   input <- 
-    lapply(input_sep2, function(x) {x[1]:x[2]}) |> 
-    unlist()
+    lapply(input_sep2, function(x) {x[1]:x[2]}) |> # list of vectors
+    unlist()                                       # vector
+  # check for pattern
   invalid <- 
     lapply(input, ifelse(pattern == "part1", check_part1, check_part2)) |> 
     unlist()
-  # return sum
-  return(
-    input[invalid] |> sum()
-    )
+  # sum invalid values
+  return(input[invalid] |> sum())
 }
 
 #### part 1 ####
 # check if number is ONLY a sequence of digits repeated EXACTLY twice
-check_part1 <- function(values) {
+check_part1 <- function(values) { # vector
   digits <- nchar(values)
   ifelse(
-    digits %% 2 == 0, # has even number of digits
-    substr(value, 1, digits/2) == substr(value, digits/2+1, digits),
-    FALSE # odd number always FALSE
+    digits %% 2 != 0 | digits == 1, # if has odd number of digits or single digit
+    FALSE,                          # always FALSE
+    # else compare first half with second half
+    substr(values, 1, digits/2) == substr(values, digits/2+1, digits)
   )
 }
+check_part1(c(2, 1212, 9.394e+09))
 
 #### part 2 ####
 # check if number is ONLY a sequence of digits repeated AT LEAST twice
-check_part2 <- function(values) {
+# check single value against sequence of given length
+# sl: sequence length, v: value, d: digits
+test_seq <- function(sl, v, d) { 
+  test <- 
+    Reduce(
+      paste0, 
+      rep(substr(v, 1, sl), times=d/sl)
+    )
+  return(v == test)
+}
+
+check_part2 <- function(values) { # vector
   sapply(values, function(value){
     digits <- nchar(value)
     if (digits == 1) {return(FALSE)}
@@ -37,26 +49,21 @@ check_part2 <- function(values) {
     max_seq_length <- floor(digits/2)
     invalid <- Reduce(
       any, 
+      # iterate over seq length
       lapply(1:max_seq_length, test_seq, v=value, d=digits)
     )
     return(invalid)
   })
 }
-check_part2(9.394e+09)
-
-test_seq <- function(sl, v, d) {
-  test <- 
-    Reduce(
-      paste0, 
-      rep(substr(v, 1, sl), times = d/sl)
-    )
-  return(v == test)
-}
+check_part2(c(2, 1212, 9.394e+09))
 
 #### run ####
-invalid_id_sum("day2/input_example.txt", pattern = "part1")
-invalid_id_sum("day2/input.txt", pattern = "part1")
-
-invalid_id_sum("day2/input_example.txt", pattern = "part2")
-invalid_id_sum("day2/input.txt", pattern = "part2")
-
+# part 1
+invalid_id_sum("day2/input_example.txt", pattern="part1")
+invalid_id_sum("day2/input.txt", pattern="part1")
+# part 2
+invalid_id_sum("day2/input_example.txt", pattern="part2")
+invalid_id_sum("day2/input.txt", pattern="part2")
+# compare time
+invalid_id_sum("day2/input_example.txt", pattern="part1") |> system.time()
+invalid_id_sum("day2/input_example.txt", pattern="part2") |> system.time()
