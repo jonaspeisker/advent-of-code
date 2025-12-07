e9d0419e9c80999101bf3ef55cacdaa1441d7b12
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-07 11:20:22 +0100

    d7p1

---
+++ 2025/R/day7.R
@@ -0,0 +1,32 @@
+beam_splits <- function(
+    file_name="input_example.txt", 
+    path="../../Nextcloud/aoc25_inputs/day7/"
+) {
+  input <- 
+    readLines(paste0(path, file_name)) |> 
+    strsplit("")
+  
+  # iterate over rows
+  n_cols <- length(input[[1]])
+  beam_prev <- input[[1]] == "S"
+  split_counter <- 0
+  for (i in 2:length(input)) {
+    # find splitting positions and count
+    split_here <- (input[[i]] == "^") & beam_prev
+    split_ind <- which(split_here)
+    split_counter <- split_counter + length(split_ind)
+    
+    # get new beam locations
+    beam_next <- beam_prev & !split_here
+    beam_next[split_ind - 1] <- TRUE
+    beam_next[split_ind + 1] <- TRUE
+    
+    # save for next iteration
+    beam_prev <- beam_next
+  }
+  
+  message("The number of beam splits is ", split_counter)
+}
+
+beam_splits("input_example.txt")
+beam_splits("input.txt")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 🎄 Advent of Code 🎄
 
-## 2025
+## ❄️ 2025 ❄️ 
 
 1.  [Secret Entrance](2025/R/day1.R)
 1.  [Gift Shop](2025/R/day2.R)
@@ -8,3 +8,4 @@
 1.  [Printing Department](2025/R/day4.R)
 1.  [Cafeteria](2025/R/day5.R)
 1.  [Trash Compactor](2025/R/day6.R)
+1.  [Laboratories](2025/R/day7.R)
