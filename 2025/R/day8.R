84dcb324d41b1fcae480211234ec6f200dfb30dc
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-08 23:06:46 +0100

    combine d8 p1 and 2

--- 2025/R/day8.R
+++ 2025/R/day8.R
@@ -7,7 +7,6 @@ eucl_dist <- function(p, q) { # returns numeric
 get_circuits <- function(
     file_name="input_example.txt", 
     path="../../Nextcloud/aoc25_inputs/day8/",
-    part=1,
     connections=10,
     verbose=FALSE
 ) {
@@ -19,7 +18,7 @@ get_circuits <- function(
     c("b1_ind" = i[1], "b2_ind" = i[2], # b1,2: index of junctions box 
       "dist" = eucl_dist(input[i[1],], input[i[2],]))
   }) |> 
-  t() # rows to cols
+    t() # rows to cols
   # sort by ascending distance
   dists_ord <- dists[order(dists[,"dist"]),]
   
@@ -27,14 +26,13 @@ get_circuits <- function(
   circuits <- list(c(dists_ord[1,"b1_ind"], dists_ord[1,"b2_ind"]))
   boxes <- 1:nrow(input)
   unconnected_boxes <- boxes[!(boxes %in% unlist(circuits))]
-  iter <- ifelse(part == 1, connections, length(combs))
-  for (i in 2:iter) {
+  for (i in 2:length(combs)) {
     if (verbose) {message(
-        "Connection ", i, ":\n(", 
-        dists_ord[i,"b1_ind"], ") ", 
-        paste0(input[dists_ord[i,"b1_ind"],], collapse=","), 
-        " with (", dists_ord[i,"b2_ind"], ") ", 
-        paste0(input[dists_ord[i,"b2_ind"],], collapse=",")
+      "Connection ", i, ":\n(", 
+      dists_ord[i,"b1_ind"], ") ", 
+      paste0(input[dists_ord[i,"b1_ind"],], collapse=","), 
+      " with (", dists_ord[i,"b2_ind"], ") ", 
+      paste0(input[dists_ord[i,"b2_ind"],], collapse=",")
     )}
     # next closest boxes
     con <- c(dists_ord[i,"b1_ind"], dists_ord[i,"b2_ind"])
@@ -51,32 +49,27 @@ get_circuits <- function(
     } else {                     # add new circuit
       circuits <- c(circuits, list(con))
     }
-    # solve part 2
-    if (part == 2) {
-      unconnected_boxes <- unconnected_boxes[
-        !(unconnected_boxes %in% unlist(circuits))]
-      if (length(unconnected_boxes) == 0 & length(circuits) == 1) {
-        return(
-          as.numeric(input[dists_ord[i,"b1_ind"], 1]) * 
-            input[dists_ord[i,"b2_ind"], 1]
-        )
-      } 
+    
+    # solve part 1
+    if (i == connections) {
+      circuits_unq <- lapply(circuits, unique) # remove duplicate boxes
+      # sort by descending length
+      len_srt <- sapply(circuits_unq, length) |> sort(decreasing=TRUE)
+      # return product of three longest
+      message("Result for part 1: ", Reduce(`*`, len_srt[1:3]))
     }
+    
+    # solve part 2
+    unconnected_boxes <- unconnected_boxes[
+      !(unconnected_boxes %in% unlist(circuits))]
+    if (length(unconnected_boxes) == 0 & length(circuits) == 1) {
+      prod_p2 <- as.numeric(input[dists_ord[i,"b1_ind"], 1]) * 
+        input[dists_ord[i,"b2_ind"], 1]
+      message("Result for part 2: ", prod_p2)
+      return(invisible(prod_p2))
+    } 
   } # end loop
-  # solve part 1
-  if (part == 1) {
-    circuits_unq <- lapply(circuits, unique) # remove duplicate boxes
-    # sort by descending length
-    len_srt <- sapply(circuits_unq, length) |> sort(decreasing=TRUE)
-    # return product of three longest
-    message("The product is ", Reduce(`*`, len_srt[1:3]))
-  }
-}
+} 
 
-# part 1
-get_circuits(part=1, verbose=TRUE)
-get_circuits("input.txt", part=1, connections=1000)
-
-# part 2
-get_circuits(part=2, verbose=TRUE)
-get_circuits("input.txt", part=2)
+get_circuits()
+get_circuits("input.txt", connections=1000)
