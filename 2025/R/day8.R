b0c4c0020e93671b19c8cfc552910839c7458bf4
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-08 16:20:57 +0100

    d8p2

--- 2025/R/day8.R
+++ 2025/R/day8.R
@@ -34,21 +34,21 @@ get_circuits_p1 <- function(
     in_circuit <- sapply(circuits, function(x){
       any(con %in% x)
     })
-    c_ind <- which(in_circuit) # to which circuits does the new connection connect
-    n_circuit <- length(c_ind) # to how many circuits does the new connection connect
-    if (n_circuit == 1) {
+    c_ind <- which(in_circuit)   # connects to which circuits
+    n_circuit <- length(c_ind)   # to how many circuits
+    if (n_circuit == 1) {        # add to circuit
       circuits[[c_ind]] <- c(circuits[[c_ind]], con)
-    } else if (n_circuit == 2) {
+    } else if (n_circuit == 2) { # merge circuits
       circuits[[c_ind[1]]] <- c(circuits[[c_ind[1]]], circuits[[c_ind[2]]], con)
       circuits[[c_ind[2]]] <- NULL
-    } else {
+    } else {                     # add new circuit
       circuits <- c(circuits, list(con))
     }
   }
-  # circuits
-  circuits_unq <- lapply(circuits, unique)
+  circuits_unq <- lapply(circuits, unique) # remove duplicate boxes
+  # sort by descending length
   len_srt <- sapply(circuits_unq, length) |> sort(decreasing=TRUE)
-  
+  # return product of three longest
   message("The product is ", Reduce(`*`, len_srt[1:3]))
 }
 
@@ -58,7 +58,8 @@ get_circuits_p1("input.txt", connections=1000)
 #### part 2 ####
 get_circuits_p2 <- function(
     file_name="input_example.txt", 
-    path="../../Nextcloud/aoc25_inputs/day8/"
+    path="../../Nextcloud/aoc25_inputs/day8/",
+    verbose=FALSE
 ) {
   input <- read.csv(paste0(path, file_name), header=F)
   # get combinations
@@ -73,37 +74,42 @@ get_circuits_p2 <- function(
   dists_ord <- dists[order(dists[,"dist"]),]
   
   circuits <- list(c(dists_ord[1,"b1_ind"], dists_ord[1,"b2_ind"]))
-  # message("Connecting ", input[dists_ord[1,"b1_ind"],] |> paste0(collapse=","),
-  #         " with ", input[dists_ord[1,"b2_ind"],] |> paste0(collapse=","))
-  for (i in 2:connections) {
+  boxes <- 1:nrow(input)
+  n_boxes <- length(boxes)
+  unconnected_boxes <- boxes[!(boxes %in% unlist(circuits))]
+  for (i in 2:length(combs)) {#
+    # i <- 11
     # print(i)
-    # message("Connecting ", input[dists_ord[i,"b1_ind"],] |> paste0(collapse=","),
-    #         " with ", input[dists_ord[i,"b2_ind"],] |> paste0(collapse=","))
-    # i <- 10
+    if (verbose) {
+      message(
+        "Connection ", i, ":\n(", 
+        dists_ord[i,"b1_ind"], ") ", input[dists_ord[i,"b1_ind"],] |> paste0(collapse=","), 
+        " with (", dists_ord[i,"b2_ind"], ") ", input[dists_ord[i,"b2_ind"],] |> paste0(collapse=","))
+    }
     con <- c(dists_ord[i,"b1_ind"], dists_ord[i,"b2_ind"])
     in_circuit <- sapply(circuits, function(x){
       any(con %in% x)
     })
-    c_ind <- which(in_circuit) # to which circuits does the new connection connect
-    n_circuit <- length(c_ind) # to how many circuits does the new connection connect
-    if (n_circuit == 1) {
+    c_ind <- which(in_circuit)   # connects to which circuits
+    n_circuit <- length(c_ind)   # to how many circuits
+    if (n_circuit == 1) {        # add to circuit
       circuits[[c_ind]] <- c(circuits[[c_ind]], con)
-    } else if (n_circuit == 2) {
+    } else if (n_circuit == 2) { # merge circuits
       circuits[[c_ind[1]]] <- c(circuits[[c_ind[1]]], circuits[[c_ind[2]]], con)
       circuits[[c_ind[2]]] <- NULL
-    } else {
+    } else {                     # add new circuit
       circuits <- c(circuits, list(con))
     }
+    # circuits_unq <- lapply(circuits, unique)
+    unconnected_boxes <- unconnected_boxes[!(unconnected_boxes %in% unlist(circuits))]
+
+    if (length(unconnected_boxes) == 0 & length(circuits) == 1) {
+      prod <- as.numeric(input[dists_ord[i,"b1_ind"], 1]) * input[dists_ord[i,"b2_ind"], 1]
+      return(prod)
+    } 
   }
-  # circuits
-  circuits_unq <- lapply(circuits, unique)
-  len_srt <- sapply(circuits_unq, length) |> sort(decreasing=TRUE)
-  
-  message("The product is ", Reduce(`*`, len_srt[1:3]))
 }
 
-
-get_circuits(connections=10)
-get_circuits("input.txt", connections=1000)
-
+get_circuits_p2(verbose=TRUE)
+get_circuits_p2("input.txt")
 
