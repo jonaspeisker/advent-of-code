7ae3a5947032c3d26f79ddb38fe943153d01516c
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-13 12:56:42 +0100

    vectorize d1

--- 2025/R/day1.R
+++ 2025/R/day1.R
@@ -1,64 +1,47 @@
-#### main ####
-d1 <- function(day=1, example=T, any_pass=F) {
+d1 <- function(
+    day = 1, 
+    example = TRUE, 
+    start_pos = 50, 
+    any_pass = FALSE
+  ) {
   verbose <- example
-  input <- 
-    get_file_name(day, example) |>
-    read.table(col.names="turn")
-  input$dir <- input$turn |> substr(1, 1) # direction
-  input$dist <- input$turn |> substr(2, 100) |> as.integer() # distance
+  turns <- # vector
+    get_file_name(day, example) |> 
+    readLines()
+  dist_full <- turns |> substr(2, 100) |> as.integer() # distance
+  dist_mod  <- dist_full %% 100 # remainder of division by 100
+  turn_left <- substr(turns, 1, 1) == "L" # direction 
+  dist_mod_incr <- ifelse(turn_left, -dist_mod, dist_mod) # subtract if left
   
-  # turn dial 
-  counter <- 0
-  position <- 50
-  previous_position <- 50
-  for (i in seq_along(input$turn)) {
-    zeros <- 0
-    turns_mod <- input$dist[i] %% 100
-    # times passed 0 (hundreds)
-    if (any_pass) {
-      zeros <- (input$dist[i] - turns_mod) / 100
-    }
-    
-    if (input$dir[i] == "L") {     # left
-      pos_tmp <- position - turns_mod
-      if (pos_tmp >= 0) {       # not cross 0
-        position <- pos_tmp
-      } else {                  # cross 0
-        position <- pos_tmp + 100
-        if (any_pass & previous_position != 0) {
-          zeros <- zeros + 1
-        }
-      }
-    } else {                    # right
-      pos_tmp <- position + turns_mod
-      if (pos_tmp <= 99) {      # not cross 0
-        position <- pos_tmp
-      } else {                  # cross 0
-        position <- pos_tmp - 100
-        if (any_pass & position != 0 & previous_position != 0) {
-          zeros <- zeros + 1
-        }
-      }
-    }
-    # current position 0
-    zeros <- zeros + (position == 0) 
-    
-    # add zeros of current turn
-    counter <- counter + zeros
-    previous_position <- position 
-    if (verbose) {
-      message(
-        "Position after turn ", input$turn[i], ": ", position, 
-        " (", zeros, " zeros)"
-      )
-    }
+  # get dial positions
+  # cumulative position
+  dial_pos_raw <- c(start_pos, start_pos + cumsum(dist_mod_incr)) 
+  dial_pos <- dial_pos_raw %% 100   # dial positions in 0–99 range
+  # to determine whether 0 or 100 was crossed
+  turn_raw <- dial_pos[-length(dial_pos)] + dist_mod_incr 
+  if (verbose) { cat("Dial positions:\n", dial_pos) }
+  
+  # count zeros
+  is_zero <- dial_pos == 0              # is current position zero
+  was_zero <- is_zero[-length(is_zero)] # was previous position zero
+  if (!any_pass) {
+    zeros <- sum(is_zero)
+    message("The password is: ", zeros)
+    return(zeros)
+  } else {
+    crosses_zero <- 
+      ( turn_left & turn_raw < 0   & !was_zero & !is_zero[-1]) | 
+      (!turn_left & turn_raw > 100 & !was_zero & !is_zero[-1])
+    hundreds <- dist_full %/% 100 # number of full hundreds
+    zeros <- sum(is_zero, crosses_zero, hundreds)
+    message("The password is: ", zeros)
+    return(zeros)
   }
-  message("The password is ", counter)
 }
 
 # part 1
-d1(example=T, any_pass=F)
-d1(example=F, any_pass=F)
+d1(example = TRUE, any_pass = FALSE) == 3
+d1(example = FALSE, any_pass = FALSE) == 1120
 # part 2
-d1(example=T, any_pass=T)
-d1(example=F, any_pass=T)
+d1(example = TRUE, any_pass = TRUE) == 6
+d1(example = FALSE, any_pass = TRUE) == 6554
