70b188893ad81631a29afeee17a7c7b0922cd8d2
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-20 12:48:02 +0100

    simplify 2025 d7

--- R/2025/2025_day7.R
+++ R/2025/2025_day7.R
@@ -14,21 +14,20 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
   beam_prev <- input[1, ] == "S"
   split_counter <- 0L
   beams <- beam_prev
-  for (i in 2:nrow(input)) {
+  for (i in 2:n_rows) {
     # find splitting positions and count
-    # i <- 5
     split_here <- (input[i, ] == "^") & beam_prev
     split_ind <- which(split_here)
     split_counter <- split_counter + length(split_ind)
-
+    
     # get new beam locations
     beam_next <- beam_prev & !split_here
     beam_next[split_ind - 1] <- TRUE
     beam_next[split_ind + 1] <- TRUE
     
     # save for next iteration
+    beams <- rbind(beams, beam_next)
     beam_prev <- beam_next
-    beams <- rbind(beams, beam_prev)
   }
   
   if (verbose) {
@@ -38,7 +37,6 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
       cbind("\n") |>
       apply(1, paste0, collapse = " ") |> 
       cat()
-    
     cat(p)
   }
   
@@ -46,49 +44,39 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
   if (part == 1) { return(split_counter) }
   
   if (part == 2) {
-  # adjecency list
-  adj <- list()
-  for (r in seq_len(n_rows)) {
-    for (c in seq_len(n_cols)) {
-
-      if (!beams[r, c]) next
-      
-      node <- paste0(r, ",", c)
-      children <- character(0)
-      next_row <- r + 1
-      next_col <- c
-      if (next_row <= n_rows && next_col >= 1 && next_col <= n_cols) {
-        if (beams[next_row, next_col]) {
-          child <- paste0(next_row, ",", next_col)
-          adj[[node]] <- child
-          next
-        }
-      } #else {
-      #   child <- paste0(next_row, ",", next_col + c(-1, 1))
-      #   adj[[node]] <- child
-      # }
-      
-      # only add if no path down
-      for (dc in c(-1, 1)) {
-        next_col <- c + dc
-        if (next_row <= n_rows && next_col >= 1 && next_col <= n_cols) {
-          if (beams[next_row, next_col]) {
-            child <- paste0(next_row, ",", next_col)
-            children <- c(children, child)
+    # make adjacency list
+    # name: node
+    # value: children of node
+    adj <- list()
+    for (r in seq_len(n_rows - 1)) { # omit last row
+      for (c in seq_len(n_cols)) {
+        
+        if ( beams[r, c] ) { # is beam
+          node <- paste0(r, ",", c)
+          next_row <- r + 1
+          if ( beams[next_row, c] ) { # not split
+            adj[[node]] <- paste0(next_row, ",", c)
+          } else {                    # split
+            # add check for cols out of bounds to generalize
+            adj[[node]] <- paste0(next_row, ",", c + c(-1, 1))
           }
         }
+        
       }
-      adj[[node]] <- children
     }
-  }
-
-  # adj
-  start_node <- paste0("1,", which(beams[1, ])) # single start node
-  end_nodes <- paste0(n_rows, ",", which(beams[n_rows, ]))
-  memo_env <- new.env(parent = emptyenv())
-  n_paths <- count_paths(node = start_node, end_nodes = end_nodes, adj = adj, memo = memo_env)
-  # memo_env |> View()
-  return(n_paths)
+    
+    # count paths recursively
+    start_node <- paste0("1,", which(beams[1, ])) # single start node
+    end_nodes <- paste0(n_rows, ",", which(beams[n_rows, ])) # multiple end nodes
+    memo_env <- new.env(parent = emptyenv())
+    n_paths <- count_paths(
+      node = start_node, 
+      end_nodes = end_nodes, 
+      adj = adj, 
+      memo = memo_env
+    )
+    
+    return(n_paths)
   }
 }
 
@@ -117,7 +105,12 @@ count_paths <- function(node, end_nodes, adj, memo) {
   total <- 0
   for (child in children) {
     total <- total + 
-      count_paths(node = child, end_nodes = end_nodes, adj = adj, memo = memo)
+      count_paths(
+        node = child, # new start node is child -> recursion
+        end_nodes = end_nodes, 
+        adj = adj, 
+        memo = memo
+      )
   }
   
   # 4. Store and return
@@ -128,12 +121,14 @@ count_paths <- function(node, end_nodes, adj, memo) {
 # part 1
 d7(example = TRUE, part = 1) == 21
 d7(example = FALSE, part = 1) == 1546
+
 # part 2
 d7(example = TRUE, part = 2) == 40
 d7(example = FALSE, part = 2) == 13883459503480
+
 # benchmark
-microbenchmark(
-  d7(example = FALSE, part = 1), #   2.8 ms
-  d7(example = FALSE, part = 2), # 311.5 ms
+microbenchmark::microbenchmark(
+  d7(example = FALSE, part = 1), #   2.4 ms
+  d7(example = FALSE, part = 2), # 288.3 ms
   times = 10
 )
