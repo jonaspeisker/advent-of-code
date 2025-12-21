e269af88ca1950cf3d04f0831c51d304d00b2859
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-21 13:09:53 +0100

    d11

--- R/2025/2025_day11.R
+++ R/2025/2025_day11.R
@@ -2,31 +2,49 @@
 
 d11 <- function(day = 11, year = 2025, example = TRUE, part = 1) {
   verbose <- example
-  input <- read_input(day, year, example)
-
-
-  if (part == 1) {
+  input <- read_split(day, year, example, example_part = part, split = ": ")
   
-    return()
+  # make adjacency list
+  names(input) <- input |> sapply(`[`, 1) # set first element to name (node)
+  # remove first element and split (children)
+  adj_list <- input |> lapply(\(x){ strsplit(x[-1], " ")[[1]] }) 
+  
+  if (part == 1) { # How many different paths lead from you to out?
+    nodes <- c("you", "out")
+  }
+  if (part == 2) { # How many paths from svr to out visit both dac and fft?
+    nodes <- c("svr", "fft", "dac", "out")
   }
   
-  if (part == 2) {
-  
-    return()
+  n_paths <- c()
+  for (i in seq_len(length(nodes) - 1)) {
+    # count paths recursively
+    start_node <- nodes[i] # single start node
+    end_nodes <- nodes[i + 1] # multiple end nodes
+    memo_env <- new.env(parent = emptyenv())
+    n_paths_tmp <- dfs_count_paths( # R/utils/dfs_search.R
+      node = start_node, 
+      end_nodes = end_nodes, 
+      adj = adj_list, 
+      memo = memo_env
+    )
+    n_paths <- c(n_paths, n_paths_tmp)
+    
   }
+  return(Reduce(`*`, n_paths))
 }
 
 # part 1
-d11(example = TRUE,  part = 1) ==
-d11(example = FALSE, part = 1)
+d11(example = TRUE,  part = 1) == 5
+d11(example = FALSE, part = 1) == 764
 
 # part 2
-d11(example = TRUE,  part = 2) ==
-d11(example = FALSE, part = 2)
+d11(example = TRUE,  part = 2) == 2
+d11(example = FALSE, part = 2) == 462444153119850
 
 # benchmark
 microbenchmark::microbenchmark(
-  d11(example = FALSE, part = 1),
-  d11(example = FALSE, part = 2)
+  d11(example = FALSE, part = 1), #  5 ms
+  d11(example = FALSE, part = 2)  # 20 ms
 )
 
--- R/2025/2025_day7.R
+++ R/2025/2025_day7.R
@@ -47,7 +47,7 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
     # make adjacency list
     # name: node
     # value: children of node
-    adj <- list()
+    adj_list <- list()
     for (r in seq_len(n_rows - 1)) { # omit last row
       for (c in seq_len(n_cols)) {
         
@@ -55,10 +55,10 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
           node <- paste0(r, ",", c)
           next_row <- r + 1
           if ( beams[next_row, c] ) { # not split
-            adj[[node]] <- paste0(next_row, ",", c)
+            adj_list[[node]] <- paste0(next_row, ",", c)
           } else {                    # split
             # add check for cols out of bounds to generalize
-            adj[[node]] <- paste0(next_row, ",", c + c(-1, 1))
+            adj_list[[node]] <- paste0(next_row, ",", c + c(-1, 1))
           }
         }
         
@@ -69,54 +69,15 @@ d7 <- function(day = 7, year = 2025, example = TRUE, part = 1) {
     start_node <- paste0("1,", which(beams[1, ])) # single start node
     end_nodes <- paste0(n_rows, ",", which(beams[n_rows, ])) # multiple end nodes
     memo_env <- new.env(parent = emptyenv())
-    n_paths <- count_paths(
+    n_paths <- dfs_count_paths( # R/utils/dfs_search.R
       node = start_node, 
       end_nodes = end_nodes, 
-      adj = adj, 
+      adj = adj_list, 
       memo = memo_env
     )
     
     return(n_paths)
   }
-}
-
-#### count paths ####
-count_paths <- function(node, end_nodes, adj, memo) {
-  
-  # 1. Memo lookup
-  if (exists(node, memo, inherits = FALSE)) {
-    return(memo[[node]])
-  }
-  
-  # 2. Base case: node is an end node
-  if (node %in% end_nodes) {
-    memo[[node]] <- 1
-    return(1)
-  }
-  
-  # 3. Recursive case
-  children <- adj[[node]]
-  
-  # If no children, this is a dead end
-  if (length(children) == 0) {
-    memo[[node]] <- 0
-    return(0)
-  }
-  
-  total <- 0
-  for (child in children) {
-    total <- total + 
-      count_paths(
-        node = child, # new start node is child -> recursion
-        end_nodes = end_nodes, 
-        adj = adj, 
-        memo = memo
-      )
-  }
-  
-  # 4. Store and return
-  memo[[node]] <- total
-  return(total)
 }
 
 #### run ####
