aac1ffcc7b5111f386873a9b64226f45fd551bd2
Jonas Peisker <peisker@iiasa.ac.at>
2025-12-30 11:01:05 +0100

    d8

--- R/2015/2015_day8.R
+++ R/2015/2015_day8.R
@@ -1,56 +1,49 @@
 # https://adventofcode.com/2015/day/8
 
 d8 <- function(day = 8, year = 2015, example = FALSE, part = 1) {
-  verbose <- example
   input <- read_input(day, year, example)
+  # number of chars in code representation of the string literal
   nchar_literal <- nchar(input)
   
+  # what is the number of characters of code for string literals 
+  # minus the number of characters in memory for the values of the strings?
   if (part == 1) {
-    hex_regex <- "\\\\x[0-9A-Fa-f]{2}"
-    input_sub <- 
-      input |>
-      gsub(
-        pattern = "\\\"",
-        replacement = "\'",
-        fixed = TRUE
-      ) |> 
-      gsub(
-        pattern = "\\\\",
-        replacement = "\\",
-        fixed = TRUE
-      )
-
-    hex_subtr <- 
-      regmatches(input, gregexpr("\\\\x[0-9A-Fa-f]{2}", input)) |> 
-      sapply(\(x) length(x) * 3 )
-
-    nchar_mem <- nchar(input_sub) - 2 - hex_subtr
+    n_esc <- 
+      input |> # match \\\"
+      gregexpr(pattern = "\\\\[\\\\\"]") |> 
+      regmatches(x = input) |> 
+      lengths() # short for sapply(x, length)
+    n_hex <- 
+      input |> # match \x01
+      gregexpr(pattern = "\\\\x[0-9A-Fa-f]{2}") |> 
+      regmatches(x = input) |> 
+      lengths() 
     
+    # 2: enclosing quotes
+    # n_esc: number of escaped characters
+    # n_hex: number of hex ASCII codes
+    nchar_mem <- nchar_literal - 2 - n_esc - 3*n_hex
     return(sum(nchar_literal) - sum(nchar_mem))
   }
   
+  # the total number of characters to represent the newly encoded strings 
+  # minus the number of characters of code in each original string literal
   if (part == 2) {
-    input_sub <- 
-      input |>
-      gsub(
-        pattern = "\\",       # 1 escaped backslash
-        replacement = "\\\\", # 2 escaped backslashes
-        fixed = TRUE
-      ) |>
-      gsub(
-        pattern = "\\\"",
-        replacement = "\\\\'",
-        fixed = TRUE
-      )
-    
-  nchar_mem <- nchar(input_sub) + 4 
-    
-  return(sum(nchar_mem) - sum(nchar_literal))
+    n_quote <- 
+      input |> # match "
+      gregexpr(pattern = "\"") |> 
+      regmatches(x = input) |> 
+      lengths()
+    n_slash <- 
+      input |> # match \\
+      gregexpr(pattern = "\\\\") |> 
+      regmatches(x = input) |> 
+      lengths()
+
+    nchar_mem <- nchar_literal + n_quote + n_slash + 2    
+    return(sum(nchar_mem) - sum(nchar_literal))
   }
 }
-
-cat(input)
-cat(input_sub)
 
 # part 1
 d8(example = TRUE,  part = 1) == 12
@@ -58,23 +51,10 @@ d8(example = FALSE, part = 1) == 1333
 
 # part 2
 d8(example = TRUE,  part = 2) == 19
-d8(example = FALSE, part = 2)
+d8(example = FALSE, part = 2) == 2046
 
 # benchmark
 microbenchmark::microbenchmark(
-  d8(example = FALSE, part = 1),
-  d8(example = FALSE, part = 2)
+  d8(example = FALSE, part = 1), # 3.7 ms
+  d8(example = FALSE, part = 2)  # 4.5 ms
 )
-
-
-
-hex_to_char <- function(x) {
-  regmatches(x, gregexpr("\\\\x[0-9A-Fa-f]{2}", x)) |> 
-    lapply(function(x) {
-      x |> 
-        substr(3, 5) |> 
-        strtoi(16) |> 
-        as.raw() |> 
-        rawToChar()
-    })
-}
